7ebb34d901826b7b8eb9db2f5df9ddfdfaa90d71
m.takuma <m.takuma.dev@gmail.com>
2023-05-14 22:23:11 +0900

    ディレクトリ構成の修正・docsの追加

--- src/const.py
+++
@@ -1,2 +0,0 @@
-EDINET_API_ENDPOINT_BASE: str = "https://disclosure.edinet-fsa.go.jp/api/v1/"
-EDINET_API_ENDPOINT_DOCS: str = EDINET_API_ENDPOINT_BASE + "documents.json"
---
+++ src/edinet_api/const.py
@@ -0,0 +1,2 @@
+EDINET_API_ENDPOINT_BASE: str = "https://disclosure.edinet-fsa.go.jp/api/v1/"
+EDINET_API_ENDPOINT_DOCS: str = EDINET_API_ENDPOINT_BASE + "documents.json"
